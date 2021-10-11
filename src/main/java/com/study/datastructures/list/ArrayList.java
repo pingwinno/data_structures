5a0728903ae7785bfd65bd6006ce80f6af56cba5
Your Name <you@example.com>
2021-10-11 13:40:36 -0700

    Cleanup dependencies Add expand array test

--- src/main/java/com/study/datastructures/list/ArrayList.java
+++ src/main/java/com/study/datastructures/list/ArrayList.java
@@ -134,7 +134,7 @@ public class ArrayList implements List {
     public String toString() {
         StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
         for (int i = 0; i < size; i++) {
-            stringJoiner.add(array[i].toString());
+            stringJoiner.add(String.valueOf(array[i]));
         }
         return stringJoiner.toString();
     }
