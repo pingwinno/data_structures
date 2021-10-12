84b004e8ed73123b7eb4de2a904e03953cdb510b
Your Name <you@example.com>
2021-10-12 05:35:32 -0700

    Add LinkedList

---
+++ src/main/java/com/study/datastructures/list/LinkedList.java
@@ -0,0 +1,165 @@
+package com.study.datastructures.list;
+
+import java.util.Objects;
+
+public class LinkedList implements List {
+
+    private int size;
+    private Node head;
+    private Node tail;
+
+    public LinkedList() {
+    }
+
+    @Override
+    public void add(Object value) {
+        add(value, size);
+    }
+
+    @Override
+    public void add(Object value, int index) {
+        if (index > size) {
+            throw new IndexOutOfBoundsException("Can't add new element. Index: " + index + " is bigger than size: " + size);
+        }
+        var newNode = new Node(value);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else if (index == 0) {
+            newNode.nextNode = head;
+            head = newNode;
+        } else if (index == size) {
+            newNode.previousNode = tail;
+            tail.nextNode = newNode;
+            tail = newNode;
+        } else {
+            var targetNode = getNodeByIndex(index);
+            newNode.previousNode = targetNode.previousNode;
+            newNode.nextNode = targetNode;
+            targetNode.previousNode.nextNode = newNode;
+            targetNode.previousNode = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public Object remove(int index) {
+        isIndexValid(index);
+        var node = getNodeByIndex(index);
+        var nextNode = node.nextNode;
+        var previousNode = node.previousNode;
+        if (nextNode != null) {
+            nextNode.previousNode = previousNode;
+        }
+        if (previousNode != null) {
+            previousNode.nextNode = nextNode;
+        }
+        node.nextNode = node.previousNode = null;
+        size--;
+        return node.value;
+    }
+
+    @Override
+    public Object get(int index) {
+        isIndexValid(index);
+        if (index == 0) {
+            return head.value;
+        }
+        return getNodeByIndex(index).value;
+    }
+
+    @Override
+    public Object set(Object value, int index) {
+        isIndexValid(index);
+        var node = getNodeByIndex(index);
+        var oldValue = node.value;
+        node.value = value;
+        return oldValue;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return indexOf(value) != -1;
+    }
+
+    @Override
+    public int indexOf(Object value) {
+        var node = head;
+        int currentIndex = 0;
+        while (node != null) {
+            if (Objects.equals(value, node.value)) {
+                return currentIndex;
+            }
+            node = node.nextNode;
+            currentIndex++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object value) {
+        var node = tail;
+        int currentIndex = size - 1;
+        while (node != null) {
+            if (Objects.equals(value, node.value)) {
+                return currentIndex;
+            }
+            node = node.previousNode;
+            currentIndex--;
+        }
+        return -1;
+    }
+
+    private Node getNodeByIndex(int index) {
+        Node node = null;
+        if (index <= size / 2) {
+            node = head;
+            int currentIndex = 0;
+            while (node.nextNode != null && currentIndex < index) {
+                node = node.nextNode;
+                currentIndex++;
+            }
+        } else if (index >= size / 2) {
+            node = tail;
+            int currentIndex = size - 1;
+            while (node.previousNode != null && currentIndex > index) {
+                node = node.previousNode;
+                currentIndex--;
+            }
+        }
+        return node;
+    }
+
+    private void isIndexValid(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException("index: " + index + " out of list size:" + size);
+        }
+    }
+
+    private static class Node {
+        private Node previousNode;
+        private Node nextNode;
+        private Object value;
+
+        public Node(Object value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/com/study/datastructures/list/LinkedListTest.java
@@ -0,0 +1,187 @@
+package com.study.datastructures.list;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class LinkedListTest {
+
+    private LinkedList linkedList;
+
+    @BeforeEach
+    void init() {
+        linkedList = new LinkedList();
+    }
+
+    @Test
+    void addOneElement() {
+        var expectedObject = 1;
+        var expectedSize = 1;
+        linkedList.add(expectedObject);
+        assertEquals(expectedObject, linkedList.get(0));
+        assertEquals(expectedSize, linkedList.size());
+    }
+
+    @Test
+    void addMultipleElements() {
+        var expectedSize = 2;
+        var firstExpectedObject = 1;
+        var secondExpectedObject = 2;
+        linkedList.add(firstExpectedObject);
+        linkedList.add(secondExpectedObject);
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(secondExpectedObject, linkedList.get(1));
+        assertEquals(expectedSize, linkedList.size());
+    }
+
+    @Test
+    void addMultipleElementsByIndexToListTail() {
+        var expectedSize = 2;
+        var firstExpectedObject = 1;
+        var secondExpectedObject = 2;
+        linkedList.add(firstExpectedObject);
+        linkedList.add(secondExpectedObject, 1);
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(secondExpectedObject, linkedList.get(1));
+        assertEquals(expectedSize, linkedList.size());
+    }
+
+    @Test
+    void addMultipleElementsByIndex() {
+        var expectedSize = 3;
+        var firstExpectedObject = 1;
+        var secondExpectedObject = 2;
+        linkedList.add(firstExpectedObject);
+        linkedList.add(secondExpectedObject);
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(secondExpectedObject, linkedList.get(1));
+        var thirdExpectedObject = 3;
+        linkedList.add(thirdExpectedObject,1);
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(thirdExpectedObject, linkedList.get(1));
+        assertEquals(secondExpectedObject, linkedList.get(2));
+        assertEquals(expectedSize, linkedList.size());
+    }
+
+    @Test
+    void addElementsByIndexWithOutOutBoundsException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.add(new Object(), 10));
+    }
+
+    @Test
+    void remove() {
+        var expectedSize = 0;
+        var expectedObject = new Object();
+        linkedList.add(expectedObject);
+        assertEquals(expectedObject, linkedList.get(0));
+        assertEquals(expectedObject, linkedList.remove(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(0));
+        assertEquals(expectedSize, linkedList.size());
+    }
+
+    @Test
+    void removeWithMultipleElements() {
+        var expectedSize = 2;
+        var firstExpectedObject = 1;
+        var secondExpectedObject = 2;
+        var thirdExpectedObject = 3;
+        linkedList.add(firstExpectedObject);
+        linkedList.add(secondExpectedObject);
+        linkedList.add(thirdExpectedObject);
+        assertEquals(secondExpectedObject, linkedList.get(1));
+        assertEquals(secondExpectedObject, linkedList.remove(1));
+        assertEquals(expectedSize, linkedList.size());
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(thirdExpectedObject, linkedList.get(1));
+    }
+
+    @Test
+    void removeWithOutOutBoundsException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.remove(0));
+    }
+
+    @Test
+    void getWithOutOutBoundsException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(0));
+    }
+
+    @Test
+    void set() {
+        var oldObject = new Object();
+        linkedList.add(oldObject);
+        assertEquals(oldObject, linkedList.get(0));
+        var newObject = new Object();
+        linkedList.set(newObject, 0);
+        assertEquals(newObject, linkedList.get(0));
+    }
+
+    @Test
+    void clear() {
+        var expectedSize = 2;
+        var firstExpectedObject = new Object();
+        var secondExpectedObject = new Object();
+        linkedList.add(firstExpectedObject);
+        linkedList.add(secondExpectedObject);
+        assertEquals(firstExpectedObject, linkedList.get(0));
+        assertEquals(secondExpectedObject, linkedList.get(1));
+        assertEquals(expectedSize, linkedList.size());
+        linkedList.clear();
+        assertEquals(0, linkedList.size());
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(1));
+    }
+
+    @Test
+    void isEmpty() {
+        assertTrue(linkedList.isEmpty());
+    }
+
+    @Test
+    void isNotEmpty() {
+        linkedList.add(new Object());
+        assertFalse(linkedList.isEmpty());
+    }
+
+    @Test
+    void contains() {
+        var expectedObject = new Object();
+        linkedList.add(expectedObject);
+        assertTrue(linkedList.contains(expectedObject));
+    }
+
+    @Test
+    void indexOf() {
+        var expectedIndex = 1;
+        var expectedObject = new Object();
+        linkedList.add(new Object());
+        linkedList.add(expectedObject);
+        linkedList.add(new Object());
+        linkedList.add(expectedObject);
+        linkedList.add(new Object());
+        assertEquals(expectedIndex, linkedList.indexOf(expectedObject));
+    }
+
+    @Test
+    void indexOfWithOneElement() {
+        var expectedIndex = 0;
+        var expectedObject = new Object();
+        linkedList.add(expectedObject);
+        assertEquals(expectedIndex, linkedList.indexOf(expectedObject));
+    }
+
+    @Test
+    void lastIndexOf() {
+        var expectedIndex = 3;
+        var expectedObject = new Object();
+        linkedList.add(new Object());
+        linkedList.add(expectedObject);
+        linkedList.add(new Object());
+        linkedList.add(expectedObject);
+        linkedList.add(new Object());
+        assertEquals(expectedIndex, linkedList.lastIndexOf(expectedObject));
+    }
+}
