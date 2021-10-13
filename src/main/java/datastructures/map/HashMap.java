5619d37f6d382c81e2f808a49efea9526ed6eda4
mromanc <you@example.com>
2021-10-13 01:05:11 -0700

    Create HashMap

---
+++ src/main/java/datastructures/map/HashMap.java
@@ -0,0 +1,28 @@
+package datastructures.map;
+
+public class HashMap implements Map{
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+}
