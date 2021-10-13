fb418ae5e67401bc80d764db0ebad07691670ac5
mromanc <you@example.com>
2021-10-13 01:05:38 -0700

    Create HashMapTest

---
+++ src/test/java/datastructures/map/HashMapTest.java
@@ -0,0 +1,28 @@
+package datastructures.map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashMapTest {
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
